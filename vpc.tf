cdef275ab26cf867292c0ab15dc7c4f57f69f1d9
Kanchimoe <github@chiyo-chan.me>
2021-07-13 21:05:17 +0100

    replace vpc with a module instead

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,19 @@
 # VPC
 
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "main_vpc"
+  cidr = "10.0.0.0/16"
+
+   azs = ["us-east-1a", "us-east-1b"]
+   private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+   public_subnets = ["10.0.0.0/24", "10.0.2.0/24"]
+   
+   enable_nat_gateway = true
+   enable_vpn_gateway = true
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = module.vpc.vpc_id
 }
