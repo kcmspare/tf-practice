72f6d0c7619e50cf04445a59a2ecd5af8b5cb99b
Kanchimoe <github@chiyo-chan.me>
2021-07-15 02:00:56 +0100

    fix #3 #5 #6 #7

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 # S3 buckets
 
 resource "aws_s3_bucket" "gl_registry" {
-  bucket = "gl-registry"
+  bucket = "gl-registry-kcm"
   acl    = "private"
 }
 
@@ -11,21 +11,21 @@ resource "aws_s3_bucket" "gl_artifacts" {
 }
 
 resource "aws_s3_bucket" "gl_backup" {
-  bucket = "gl-backup"
+  bucket = "gl-backup-kcm"
   acl    = "private"
 }
 
 resource "aws_s3_bucket" "gl_lfs" {
-  bucket = "gl-lfs"
+  bucket = "gl-lfs-kcm"
   acl    = "private"
 }
 
 resource "aws_s3_bucket" "gl_uploads" {
-  bucket = "gl-uploads"
+  bucket = "gl-uploads-kcm"
   acl    = "private"
 }
 
 resource "aws_s3_bucket" "gl_mr_diffs" {
-  bucket = "gl-mr-diffs"
+  bucket = "gl-mr-diffs-kcm"
   acl    = "private"
 }
