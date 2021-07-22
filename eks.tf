995e3ef5d368ee5326ec3e7b87301e872c1d8561
Kanchimoe <github@chiyo-chan.me>
2021-07-22 05:35:24 +0100

    creation of fargate

--- eks.tf
+++ eks.tf
@@ -13,3 +13,16 @@ resource "aws_eks_cluster" "EKS_cluster" {
     aws_iam_role_policy_attachment.PolicyAttachment_Gitlab_EKS_Pods
   ]
 }
+
+# Fargate
+
+resource "aws_eks_fargate_profile" "Gitlab_EKS_fargate_profile" {
+  cluster_name            = aws_eks_cluster.EKS_cluster.id
+  fargate_profile_name    = "Gitlab_Fargate_Profile"
+  pod_execution_role_arn  = aws_iam_role.Gitlab_fargate_profile.arn
+  subnet_ids              = module.vpc.private_subnets
+
+  selector {
+    namespace = "gitlab"
+  }
+}
--- iam.tf
+++ iam.tf
@@ -27,3 +27,27 @@ resource "aws_iam_role_policy_attachment" "PolicyAttachment_Gitlab_EKS_Pods" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
     role       = aws_iam_role.Gitlab_EKS_cluster_role.name
 }
+
+resource "aws_iam_role" "Gitlab_fargate_profile" {
+    name               = "gitlab-fargate-profile"
+    description        = "Gitlab fargate profile role"
+    assume_role_policy = data.aws_iam_policy_document.AssumeRole_Gitlab_fargate_profile.json
+}
+
+data "aws_iam_policy_document" "AssumeRole_Gitlab_fargate_profile" {
+    statement {
+        actions = [
+           "sts:AssumeRole" 
+        ]
+        principals {
+            type = "Service"
+            identifiers = ["eks-fargate-pods.amazonaws.com"]
+        }
+    }
+}
+
+resource "aws_iam_role_policy_attachment" "PolicyAttachment_Gitlab_fargate_profile" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.Gitlab_fargate_profile.name
+}
+
