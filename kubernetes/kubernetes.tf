c7668e4508582fc36f69e4af967c10e7441c6f28
Kanchimoe <github@chiyo-chan.me>
2021-07-24 00:48:52 +0100

    create k8s deployment

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -8,3 +8,40 @@ resource "kubernetes_namespace" "gitlab_kubernetes_namespace" {
     }
   }
 }
+
+resource "kubernetes_deployment" "k8s_gitlab_deployment" {
+  metadata {
+    labels = {
+      use       = "Kanchimoe-Gitlab_on_AWS-K8S"
+      terraform = "true-github"
+    }
+    name      = "gitlab-k8s-deployment"
+    namespace = kubernetes_namespace.gitlab_kubernetes_namespace.metadata.0.name
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        use = "Kanchimoe-Gitlab_on_AWS-K8S"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          use = ""
+        }
+      }
+
+      spec {
+        container {
+          image = "gitlab/gitlab-ce:14.1.0-ce.0"
+          name  = "Gitlab CE 14.1.0 docker image"
+        }
+      }
+    }
+  }
+  depends_on = [kubernetes_namespace.gitlab_kubernetes_namespace]
+}
