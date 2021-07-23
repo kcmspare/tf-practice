eaf2f39feb969a4b66be16b775a63ebe37103410
Kanchimoe <github@chiyo-chan.me>
2021-07-24 00:52:14 +0100

    create iam role

---
+++ kubernetes/iam.tf
@@ -0,0 +1,68 @@
+resource "aws_iam_role" "gitlab_k8s_alb_role" {
+  name               = "gitlab_k8s_alb_role"
+  assume_role_policy = data.aws_iam_policy_document.gitlab_k8s_alb_assumerole.json
+}
+
+data "aws_iam_policy_document" "gitlab_k8s_alb_assumerole" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type = "Service"
+      identifiers = ["elasticloadbalancing.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "gitlab_k8s_alb_policyattachment" {
+  role       = aws_iam_role.gitlab_k8s_alb_role.name
+  policy_arn = aws_iam_policy.gitlab_k8s_alb_policy.arn
+}
+
+resource "aws_iam_policy" "gitlab_k8s_alb_policy" {
+  name = "gitlab_k8s_alb_policy"
+  policy = data.aws_iam_policy_document.gitlab_k8s_alb_policydocument.json
+}
+
+
+data "aws_iam_policy_document" "gitlab_k8s_alb_policydocument" {
+  statement {
+    actions = [
+      "elasticloadbalancing:AddListenerCertificates",
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:CreateListener",
+      "elasticloadbalancing:CreateLoadBalancer",
+      "elasticloadbalancing:CreateRule",
+      "elasticloadbalancing:CreateTargetGroup",
+      "elasticloadbalancing:DeleteListener",
+      "elasticloadbalancing:DeleteLoadBalancer",
+      "elasticloadbalancing:DeleteRule",
+      "elasticloadbalancing:DeleteTargetGroup",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:DescribeListenerCertificates",
+      "elasticloadbalancing:DescribeListeners",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeLoadBalancerAttributes",
+      "elasticloadbalancing:DescribeRules",
+      "elasticloadbalancing:DescribeSSLPolicies",
+      "elasticloadbalancing:DescribeTags",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetGroupAttributes",
+      "elasticloadbalancing:DescribeTargetHealth",
+      "elasticloadbalancing:ModifyListener",
+      "elasticloadbalancing:ModifyLoadBalancerAttributes",
+      "elasticloadbalancing:ModifyRule",
+      "elasticloadbalancing:ModifyTargetGroup",
+      "elasticloadbalancing:ModifyTargetGroupAttributes",
+      "elasticloadbalancing:RegisterTargets",
+      "elasticloadbalancing:RemoveListenerCertificates",
+      "elasticloadbalancing:RemoveTags",
+      "elasticloadbalancing:SetIpAddressType",
+      "elasticloadbalancing:SetSecurityGroups",
+      "elasticloadbalancing:SetSubnets",
+      "elasticloadbalancing:SetWebACL"
+    ]
+    resources = ["*"]
+  }
+}
