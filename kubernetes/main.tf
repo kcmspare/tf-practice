64f03ece449865107f259fbd370dfabc37760d41
Kanchimoe <github@chiyo-chan.me>
2021-07-24 01:19:34 +0100

    add AWS provider

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -15,3 +15,16 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.terraform_remote_state.main_tf.outputs.gitlab_eks_cluster_certificateauthority[0].data)
   token                  = data.aws_eks_cluster_auth.gitlab_eks_cluster.token
 }
+
+provider aws {
+  region = "x"
+  access_key = "x"
+  secret_key = "x"
+  
+  default_tags {
+      tags = {
+          terraform = "true/github"
+          use = "Kanchimoe/Gitlab on AWS"
+      }
+  }
+}
