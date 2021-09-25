9a2be715775f78a4f3e7af6ed4c5771ddd34d8e7
Kanchimoe <github@chiyo-chan.me>
2021-09-25 22:05:41 +0100

    add output for efs arn

--- main/outputs.tf
+++ main/outputs.tf
@@ -5,3 +5,7 @@ output "gitlab_db_redis_endpoint" {
 output "gitlab_db_redis_port" {
   value = aws_rds_cluster.DB_gitlab.port
 }
+
+output "gitlab_efs_arn" {
+  value = aws_efs_file_system.gitlab_efs.arn
+}
