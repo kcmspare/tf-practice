3ba713dbbecee6b1f80cc4efcd45ca9b3954b9c6
Kanchimoe <github@chiyo-chan.me>
2021-09-26 07:19:55 +0100

    fix spacing and constant rebuilding issue

--- main/database.tf
+++ main/database.tf
@@ -4,18 +4,21 @@ resource "aws_rds_cluster" "DB_gitlab" {
   cluster_identifier      = "aurora-cluster-gitlab"
   engine                  = "aurora-postgresql"
   engine_mode             = "serverless"
-  availability_zones      = ["us-east-1a", "us-east-1b"]
+  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1d"]
   database_name           = "gitlab_db"
   master_username         = "Should_not_be_stored_here"
   master_password         = "temp_password_for_testing_dont_really_put_stuff_here"
   backup_retention_period = 7
   preferred_backup_window = "03:00-07:00"
-  storage_encrypted =   true
-  kms_key_id = aws_kms_key.key_gitlab_db.arn
-  vpc_security_group_ids = [aws_security_group.gitlab-rds.id]
-  db_subnet_group_name = aws_db_subnet_group.gitlab_private_subnets.id
-  skip_final_snapshot = "true"
-  apply_immediately = true
+  storage_encrypted       = true
+  kms_key_id              = aws_kms_key.key_gitlab_db.arn
+  vpc_security_group_ids  = [aws_security_group.gitlab-rds.id]
+  db_subnet_group_name    = aws_db_subnet_group.gitlab_private_subnets.id
+  skip_final_snapshot     = "true"
+
+  scaling_configuration {
+    min_capacity = 2
+  }
 }
 
 resource "aws_kms_key" "key_gitlab_db" {
